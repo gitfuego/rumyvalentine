c391aed3e46ec4e3787ef9c5a9bb9f85c243257f
gitfuego <firaskhansa3@gmail.com>
2024-07-28 00:33:49 -0400

    started intergrating postgres

--- auth.js
+++ auth.js
@@ -4,10 +4,8 @@ import { Pool } from "pg"
 import Google from "next-auth/providers/google"
  
 const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  connectionString: process.env.PG_URI,
+  max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
 })
